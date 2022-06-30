0634b68940cf79b732198e5eeaf110f4df30f9ba
hyojuk <54415715+hyojuk@users.noreply.github.com>
2022-06-30 15:20:42 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export default {
-    dbYear: '2022-1',
+    dbYear: '22-23 1',
     classes: ['유치가 (금|토)', '유치나', '기초1 (금)', '기초1 (토)', '기초2 (금|토)', '기초3', '초급 (금|토)', '중급', '고급'],
     username: "bgekitljcfbdtfrkufiuvfditbehbhtv",
     auth: "fcenkjlbjujueijfrulgeghnhfhbfrre",
